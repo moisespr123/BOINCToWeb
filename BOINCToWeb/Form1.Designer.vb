44ec12efea9379407d5d7663758bb52024a96e22
Moises Cardona <cardona_76937@students.pupr.edu>
2017-04-23 18:28:42 -0400

    Add project files.

---
+++ BOINCToWeb/Form1.Designer.vb
@@ -0,0 +1,363 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.RichTextBox1 = New System.Windows.Forms.RichTextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.TextBox5 = New System.Windows.Forms.TextBox()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.TextBox6 = New System.Windows.Forms.TextBox()
+        Me.TextBox7 = New System.Windows.Forms.TextBox()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.TextBox8 = New System.Windows.Forms.TextBox()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.NumericUpDown1 = New System.Windows.Forms.NumericUpDown()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.Label11 = New System.Windows.Forms.Label()
+        Me.TextBox9 = New System.Windows.Forms.TextBox()
+        Me.Label12 = New System.Windows.Forms.Label()
+        Me.Label13 = New System.Windows.Forms.Label()
+        CType(Me.NumericUpDown1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'RichTextBox1
+        '
+        Me.RichTextBox1.Location = New System.Drawing.Point(363, 25)
+        Me.RichTextBox1.Name = "RichTextBox1"
+        Me.RichTextBox1.Size = New System.Drawing.Size(402, 325)
+        Me.RichTextBox1.TabIndex = 0
+        Me.RichTextBox1.Text = ""
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(191, 281)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(157, 23)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "Fetch Tasks!"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.Location = New System.Drawing.Point(15, 222)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(158, 147)
+        Me.ListBox1.TabIndex = 2
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(91, 13)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "BOINC PC Name:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(12, 51)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(114, 13)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "BOINC PC IP Address:"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(15, 25)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(158, 20)
+        Me.TextBox1.TabIndex = 5
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(15, 67)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(158, 20)
+        Me.TextBox2.TabIndex = 6
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(15, 159)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(158, 20)
+        Me.TextBox3.TabIndex = 8
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(12, 143)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(109, 13)
+        Me.Label3.TabIndex = 7
+        Me.Label3.Text = "BOINC PC Password:"
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(15, 185)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(158, 23)
+        Me.Button2.TabIndex = 9
+        Me.Button2.Text = "Add to List"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(12, 381)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(161, 23)
+        Me.Button3.TabIndex = 10
+        Me.Button3.Text = "Remove PC"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(187, 9)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(79, 13)
+        Me.Label4.TabIndex = 11
+        Me.Label4.Text = "MySQL Server:"
+        '
+        'TextBox4
+        '
+        Me.TextBox4.Location = New System.Drawing.Point(190, 25)
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.Size = New System.Drawing.Size(158, 20)
+        Me.TextBox4.TabIndex = 12
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(187, 51)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(67, 13)
+        Me.Label5.TabIndex = 13
+        Me.Label5.Text = "MySQL Port:"
+        '
+        'TextBox5
+        '
+        Me.TextBox5.Location = New System.Drawing.Point(190, 67)
+        Me.TextBox5.Name = "TextBox5"
+        Me.TextBox5.Size = New System.Drawing.Size(158, 20)
+        Me.TextBox5.TabIndex = 14
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(187, 99)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(94, 13)
+        Me.Label6.TabIndex = 15
+        Me.Label6.Text = "MySQL Database:"
+        '
+        'TextBox6
+        '
+        Me.TextBox6.Location = New System.Drawing.Point(190, 115)
+        Me.TextBox6.Name = "TextBox6"
+        Me.TextBox6.Size = New System.Drawing.Size(158, 20)
+        Me.TextBox6.TabIndex = 16
+        '
+        'TextBox7
+        '
+        Me.TextBox7.Location = New System.Drawing.Point(190, 159)
+        Me.TextBox7.Name = "TextBox7"
+        Me.TextBox7.Size = New System.Drawing.Size(158, 20)
+        Me.TextBox7.TabIndex = 18
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(187, 143)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(96, 13)
+        Me.Label7.TabIndex = 17
+        Me.Label7.Text = "MySQL Username:"
+        '
+        'TextBox8
+        '
+        Me.TextBox8.Location = New System.Drawing.Point(190, 207)
+        Me.TextBox8.Name = "TextBox8"
+        Me.TextBox8.Size = New System.Drawing.Size(158, 20)
+        Me.TextBox8.TabIndex = 20
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(187, 191)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(94, 13)
+        Me.Label8.TabIndex = 19
+        Me.Label8.Text = "MySQL Password:"
+        '
+        'Timer1
+        '
+        '
+        'NumericUpDown1
+        '
+        Me.NumericUpDown1.Location = New System.Drawing.Point(191, 255)
+        Me.NumericUpDown1.Name = "NumericUpDown1"
+        Me.NumericUpDown1.Size = New System.Drawing.Size(48, 20)
+        Me.NumericUpDown1.TabIndex = 21
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Location = New System.Drawing.Point(188, 239)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(111, 13)
+        Me.Label9.TabIndex = 22
+        Me.Label9.Text = "Time To Fetch Tasks:"
+        '
+        'Label10
+        '
+        Me.Label10.AutoSize = True
+        Me.Label10.Location = New System.Drawing.Point(245, 262)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(44, 13)
+        Me.Label10.TabIndex = 23
+        Me.Label10.Text = "Minutes"
+        '
+        'Label11
+        '
+        Me.Label11.AutoSize = True
+        Me.Label11.Location = New System.Drawing.Point(360, 9)
+        Me.Label11.Name = "Label11"
+        Me.Label11.Size = New System.Drawing.Size(40, 13)
+        Me.Label11.TabIndex = 24
+        Me.Label11.Text = "Status:"
+        '
+        'TextBox9
+        '
+        Me.TextBox9.Location = New System.Drawing.Point(15, 115)
+        Me.TextBox9.Name = "TextBox9"
+        Me.TextBox9.Size = New System.Drawing.Size(158, 20)
+        Me.TextBox9.TabIndex = 26
+        '
+        'Label12
+        '
+        Me.Label12.AutoSize = True
+        Me.Label12.Location = New System.Drawing.Point(12, 99)
+        Me.Label12.Name = "Label12"
+        Me.Label12.Size = New System.Drawing.Size(82, 13)
+        Me.Label12.TabIndex = 25
+        Me.Label12.Text = "BOINC PC Port:"
+        '
+        'Label13
+        '
+        Me.Label13.AutoSize = True
+        Me.Label13.Location = New System.Drawing.Point(622, 365)
+        Me.Label13.Name = "Label13"
+        Me.Label13.Size = New System.Drawing.Size(152, 39)
+        Me.Label13.TabIndex = 27
+        Me.Label13.Text = "Developed by Moisés Cardona" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "v1.0" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "http://moisescardona.me"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(777, 413)
+        Me.Controls.Add(Me.Label13)
+        Me.Controls.Add(Me.TextBox9)
+        Me.Controls.Add(Me.Label12)
+        Me.Controls.Add(Me.Label11)
+        Me.Controls.Add(Me.Label10)
+        Me.Controls.Add(Me.Label9)
+        Me.Controls.Add(Me.NumericUpDown1)
+        Me.Controls.Add(Me.TextBox8)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.TextBox7)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.TextBox6)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.TextBox5)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.TextBox4)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.RichTextBox1)
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "Form1"
+        Me.Text = "BOINC to Database to show tasks on web"
+        CType(Me.NumericUpDown1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents RichTextBox1 As RichTextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Label4 As Label
+    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents Label5 As Label
+    Friend WithEvents TextBox5 As TextBox
+    Friend WithEvents Label6 As Label
+    Friend WithEvents TextBox6 As TextBox
+    Friend WithEvents TextBox7 As TextBox
+    Friend WithEvents Label7 As Label
+    Friend WithEvents TextBox8 As TextBox
+    Friend WithEvents Label8 As Label
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents NumericUpDown1 As NumericUpDown
+    Friend WithEvents Label9 As Label
+    Friend WithEvents Label10 As Label
+    Friend WithEvents Label11 As Label
+    Friend WithEvents TextBox9 As TextBox
+    Friend WithEvents Label12 As Label
+    Friend WithEvents Label13 As Label
+End Class
---
+++ BOINCToWeb/Form1.vb
@@ -0,0 +1,190 @@
+﻿Imports BoincRpc
+
+Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        My.Settings.MySQLServer = TextBox4.Text
+        My.Settings.MySQLPort = TextBox5.Text
+        My.Settings.MySQLDatabase = TextBox6.Text
+        My.Settings.MySQLUsername = TextBox7.Text
+        My.Settings.MySQLPassword = TextBox8.Text
+        My.Settings.TimeToWait = NumericUpDown1.Value
+        My.Settings.Save()
+        If NumericUpDown1.Value > 0 Then
+            Timer1.Interval = NumericUpDown1.Value * 60 * 1000
+            Timer1.Start()
+            Dim NumberOfHosts As Integer = ListBox1.Items.Count
+            StatusLog("Starting MySQL Database Update")
+            StatusLog("Number of hosts: " & NumberOfHosts)
+            StatusLog("Starting MySQL Database Update")
+            TruncateTables(My.Settings.MySQLServer, My.Settings.MySQLPort, My.Settings.MySQLDatabase, My.Settings.MySQLUsername, My.Settings.MySQLPassword)
+            If NumberOfHosts > 0 Then
+                For i = 0 To NumberOfHosts - 1
+                    GetHostTasks(My.Settings.PCName.Item(i), My.Settings.PCIPAddress.Item(i), My.Settings.PCPort.Item(i), My.Settings.PCPassword.Item(i), My.Settings.MySQLServer, My.Settings.MySQLPort, My.Settings.MySQLDatabase, My.Settings.MySQLUsername, My.Settings.MySQLPassword)
+                Next
+            End If
+        End If
+
+    End Sub
+
+    Private Async Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim BOINCClient As New RpcClient
+        Try
+            Await BOINCClient.ConnectAsync(TextBox2.Text, TextBox9.Text)
+            Dim Authorized = Await BOINCClient.AuthorizeAsync(TextBox3.Text)
+            If Authorized = True Then
+                My.Settings.PCName.Add(TextBox1.Text)
+                My.Settings.PCIPAddress.Add(TextBox2.Text)
+                My.Settings.PCPassword.Add(TextBox3.Text)
+                My.Settings.PCPort.Add(TextBox9.Text)
+                ListBox1.Items.Add(TextBox1.Text)
+                My.Settings.Save()
+            Else
+                MsgBox("Could not connect. Please check the PC details and try again")
+            End If
+        Catch ex As Exception
+            MsgBox("Could not connect. Please check the PC details and try again")
+        End Try
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        If My.Settings.PCName Is Nothing Then
+            My.Settings.PCName = New Specialized.StringCollection
+        End If
+        If My.Settings.PCIPAddress Is Nothing Then
+            My.Settings.PCIPAddress = New Specialized.StringCollection
+        End If
+        If My.Settings.PCPassword Is Nothing Then
+            My.Settings.PCPassword = New Specialized.StringCollection
+        End If
+        If My.Settings.PCPort Is Nothing Then
+            My.Settings.PCPort = New Specialized.StringCollection
+        End If
+        If My.Settings.PCName.Count > 0 Then
+            For Each item In My.Settings.PCName
+                ListBox1.Items.Add(item)
+            Next
+        End If
+        TextBox4.Text = My.Settings.MySQLServer
+        TextBox5.Text = My.Settings.MySQLPort
+        TextBox6.Text = My.Settings.MySQLDatabase
+        TextBox7.Text = My.Settings.MySQLUsername
+        TextBox8.Text = My.Settings.MySQLPassword
+        NumericUpDown1.Value = My.Settings.TimeToWait
+    End Sub
+
+    Private Sub ListBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ListBox1.SelectedIndexChanged
+        Try
+            TextBox1.Text = ListBox1.SelectedItem
+            TextBox2.Text = My.Settings.PCIPAddress.Item(ListBox1.SelectedIndex)
+            TextBox3.Text = My.Settings.PCPassword.Item(ListBox1.SelectedIndex)
+            TextBox9.Text = My.Settings.PCPort.Item(ListBox1.SelectedIndex)
+        Catch ex As Exception
+        End Try
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        My.Settings.PCName.RemoveAt(ListBox1.SelectedIndex)
+        My.Settings.PCIPAddress.RemoveAt(ListBox1.SelectedIndex)
+        My.Settings.PCPassword.RemoveAt(ListBox1.SelectedIndex)
+        My.Settings.PCPort.RemoveAt(ListBox1.SelectedIndex)
+        ListBox1.Items.RemoveAt(ListBox1.SelectedIndex)
+        My.Settings.Save()
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        Dim NumberOfHosts As Integer = ListBox1.Items.Count
+        StatusLog("Starting MySQL Database Update")
+        TruncateTables(My.Settings.MySQLServer, My.Settings.MySQLPort, My.Settings.MySQLDatabase, My.Settings.MySQLUsername, My.Settings.MySQLPassword)
+        StatusLog("Number of hosts: " & NumberOfHosts)
+        If NumberOfHosts > 0 Then
+            For i = 0 To NumberOfHosts - 1
+                GetHostTasks(My.Settings.PCName.Item(i), My.Settings.PCIPAddress.Item(i), My.Settings.PCPort.Item(i), My.Settings.PCPassword.Item(i), My.Settings.MySQLServer, My.Settings.MySQLPort, My.Settings.MySQLDatabase, My.Settings.MySQLUsername, My.Settings.MySQLPassword)
+            Next
+        End If
+    End Sub
+    Private Sub StatusLog(text As String)
+        RichTextBox1.Text += Date.Now & " || " & text & vbNewLine
+    End Sub
+    Private Sub TruncateTables(MySQLServer As String, MySQLPort As Integer, MySQLDatabase As String, MySQLUsername As String, MySQLPassword As String)
+        StatusLog("Truncating Table")
+        Dim MySQLConnString = "server=" & MySQLServer & ";Port=" & MySQLPort & ";Database=" & MySQLDatabase & ";Uid=" & MySQLUsername & ";Pwd=" & MySQLPassword & ";Check Parameters=false;default command timeout=999;Connection Timeout=999;Pooling=false;allow user variables=true;"
+        Dim truncateSQL = "TRUNCATE TABLE tasks"
+        Dim SQLConnection = New MySql.Data.MySqlClient.MySqlConnection(MySQLConnString)
+        SQLConnection.Open()
+        Dim SQLCommand1 As New MySql.Data.MySqlClient.MySqlCommand(truncateSQL, SQLConnection)
+        SQLCommand1.ExecuteNonQuery()
+        StatusLog("Table truncated")
+    End Sub
+    Private Async Sub GetHostTasks(host As String, ip As String, port As Integer, password As String, MySQLServer As String, MySQLPort As Integer, MySQLDatabase As String, MySQLUsername As String, MySQLPassword As String)
+        StatusLog("Getting Tasks for host " & host)
+        Dim MySQLConnString = "server=" & MySQLServer & ";Port=" & MySQLPort & ";Database=" & MySQLDatabase & ";Uid=" & MySQLUsername & ";Pwd=" & MySQLPassword & ";Check Parameters=false;default command timeout=999;Connection Timeout=999;Pooling=false;allow user variables=true;"
+        Dim BOINCClient As New RpcClient
+        Try
+            Await BOINCClient.ConnectAsync(ip, port)
+            Dim Authorized As Boolean = Await BOINCClient.AuthorizeAsync(password)
+            Dim SQLInsert As String = ""
+            If Authorized Then
+                For Each result In Await BOINCClient.GetResultsAsync()
+                    Dim Percent As Double
+                    Dim Status As String = ""
+                    If result.ActiveTask = True And result.ReadyToReport = False Then
+                        If String.IsNullOrEmpty(result.PlanClass) Then
+                            Status = "Running"
+                        Else
+                            Status = "Running (" & result.PlanClass & ")"
+                        End If
+                        Percent = result.FractionDone * 100
+                    ElseIf result.ActiveTask = False And result.ReadyToReport = False Then
+                        If String.IsNullOrEmpty(result.PlanClass) Then
+                            Status = "Ready to start"
+                        Else
+                            Status = "Ready to start (" & result.PlanClass & ")"
+                        End If
+                        Percent = 0
+                    ElseIf result.ActiveTask = False And result.ReadyToReport = True Then
+                        If String.IsNullOrEmpty(result.PlanClass) Then
+                            Status = "Ready to report"
+                        Else
+                            Status = "Ready to report (" & result.PlanClass & ")"
+                        End If
+                        Percent = 100
+                    End If
+                    Dim ElapsedTime As TimeSpan
+                    If result.ElapsedTime.TotalMilliseconds = 0 Then
+                        ElapsedTime = result.FinalElapsedTime
+                    Else
+                        ElapsedTime = result.ElapsedTime
+                    End If
+
+                    Dim RemainingTime As TimeSpan = result.EstimatedCpuTimeRemaining
+                    Dim Project As String = ""
+                    If result.ProjectUrl = "http://www.worldcommunitygrid.org/" Or result.ProjectUrl = "https://www.worldcommunitygrid.org/" Then
+                        Project = "World Community Grid"
+                    ElseIf result.ProjectUrl = "https://boinc.thesonntags.com/collatz/" Or result.ProjectUrl = "http://boinc.thesonntags.com/collatz/" Then
+                        Project = "Collatz Conjecture"
+                    ElseIf result.ProjectUrl = "https://wuprop.boinc-af.org/" Or result.ProjectUrl = "http://wuprop.boinc-af.org/" Then
+                        Project = "WUProp@Home"
+                    ElseIf result.ProjectUrl = "https://moowrap.net/" Or result.ProjectUrl = "http://moowrap.net/" Then
+                        Project = "Moo! Wrapper"
+                    ElseIf result.ProjectUrl = "http://www.bitcoinutopia.net/bitcoinutopia/" Then
+                        Project = "Bitcoin Utopia"
+                    ElseIf result.ProjectUrl = "http://setiathome.berkeley.edu/" Then
+                        Project = "SETI@Home"
+                    ElseIf result.ProjectUrl = "http://asteroidsathome.net/boinc/" Then
+                        Project = "Asteroids@Home"
+                    Else
+                        Project = result.ProjectUrl
+                    End If
+                    SQLInsert += "INSERT INTO tasks (TaskName, Project, PercentDone, Status, PCName, ElapsedTime, RemainingTime, ReportDeadline) VALUES ('" & result.WorkunitName & "', '" & Project & "', '" & Percent & "', '" & Status & "', '" & host & "', '" & String.Format("{0}:{1:mm}:{1:ss}", CInt(Math.Truncate(ElapsedTime.TotalHours)), ElapsedTime) & "', '" & String.Format("{0}:{1:mm}:{1:ss}", CInt(Math.Truncate(RemainingTime.TotalHours)), RemainingTime) & "', '" & result.ReportDeadline.ToString("MM/dd/yyyy hh:mm:ss tt") & " UTC');"
+                Next
+            End If
+            Dim SQLConnection = New MySql.Data.MySqlClient.MySqlConnection(MySQLConnString)
+            SQLConnection.Open()
+            Dim SQLCommand As New MySql.Data.MySqlClient.MySqlCommand(SQLInsert, SQLConnection)
+            SQLCommand.ExecuteNonQuery()
+            StatusLog("Finished getting tasks for host " & host)
+        Catch ex As Exception
+            StatusLog("Failed getting tasks for host " & host)
+        End Try
+    End Sub
+End Class
